105a0d95e9771ca4c42d5f75880d01e5664b8d82
yose232323 <astuavalverdeyoselinepriscila@gmail.com>
2025-09-15 22:30:07 -0600

    shoping car error resolved

--- src/components/cart/ShoppingCart.jsx
+++ src/components/cart/ShoppingCart.jsx
@@ -1,3 +1,4 @@
+//src/components/cart/ShoppingCart.jsx
 "use client";
 import {
   Sheet,
@@ -7,31 +8,59 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { ShoppingBasket } from "lucide-react";
-import { Button } from "../ui/button";
 import { useCartStore } from "@/store/cart-store";
 import { Separator } from "../ui/separator";
 
 const ShoppingCart = () => {
-  const { items } = useCartStore();
+  const {
+    items,
+    removeProduct,
+    incrementQuantity,
+    getTotalItems,
+    getTotalPrice,
+  } = useCartStore();
+
+  const totalItems = getTotalItems();
+  const totalPrice = getTotalPrice();
 
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <button className="flex items-center gap-2 p-2 rounded-md hover:bg-zinc-100 transitions-color ">
+        <button className="flex items-center gap-2 p-2 rounded-md hover:bg-teal-100 trasitions-colors">
           <ShoppingBasket />
-          <span>Mi carrito</span>
+          <span>Mi Carrito</span>
         </button>
       </SheetTrigger>
 
       <SheetContent className="w-[400px] sm:w-[540px]">
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetTitle>Mi Carrito de compras</SheetTitle>
         </SheetHeader>
+        {/* ustedes deben organizar el carrito */}
         <div>
           {items.map((item) => (
-            <p>{item.title}</p>
+            <p key={item._id}>
+              {item.title}
+              <button className="bg-slate-400 p-1">-</button>
+              <span className="bg-teal-700 text-white">{item.quantity}</span>
+              <button
+                onClick={() => incrementQuantity(item.id)}
+                className="bg-slate-400 p-1"
+              >
+                +
+              </button>
+              <button
+                onClick={() => removeProduct(item.id)}
+                className="bg-red-700 text-white font-bold"
+              >
+                x
+              </button>
+            </p>
           ))}
         </div>
+        <Separator />
+        <div>Total productos:{totalItems}</div>
+        <div>Total a pagar:{totalPrice}</div>
       </SheetContent>
     </Sheet>
   );
